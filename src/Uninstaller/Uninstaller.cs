7de370ec2d84766f36cdc043b462c90b6e673e01
Strong <trongnguyenvann@gmail.com>
2025-08-31 00:09:21 +0700

    Add Uninstaller project

---
+++ src/Uninstaller/Uninstaller.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Win32;
+using System;
+using System.ComponentModel;
+using System.Configuration.Install;
+
+namespace Uninstaller
+{
+    [RunInstaller(true)]
+    public class UninstallAction : Installer
+    {
+        public override void Uninstall(System.Collections.IDictionary savedState)
+        {
+            string subKey = @"Software\Microsoft\Windows\CurrentVersion\Run";
+            // Create or open the subkey
+            using (RegistryKey key = Registry.CurrentUser.CreateSubKey(subKey))
+            {
+                if (key != null)
+                {
+                    key.DeleteValue("TakeABreak", throwOnMissingValue: false);
+                }
+            }
+        }
+    }
+}
